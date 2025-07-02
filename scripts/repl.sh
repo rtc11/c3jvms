3e862b102f590fbbcb14851b18dc51f08c7122c1
robin <robin.tordly@gmail.com>
2025-07-02 19:17:15 +0200

    add builtins

--- scripts/repl.sh
+++ scripts/repl.sh
@@ -2,5 +2,5 @@
 
 set -ex
 
-c3c compile-run scripts/repl.c3 src/lexer.c3 src/parser.c3 src/ast.c3 src/evaluator.c3 src/object.c3 src/environment.c3 src/ascii.c3
+c3c compile-run scripts/repl.c3 src/lexer.c3 src/parser.c3 src/ast.c3 src/evaluator.c3 src/object.c3 src/environment.c3 src/ascii.c3 src/builtins.c3
 
