3293897852025a7bca450edbd9516c70ef34f0a9
robin <robin.tordly@gmail.com>
2025-07-21 23:53:15 +0200

    renamed object to runtimevalue

--- scripts/repl.sh
+++ scripts/repl.sh
@@ -2,6 +2,6 @@
 
 set -ex
 
-c3c compile-run scripts/repl.c3 src/lexer.c3 src/parser.c3 src/ast.c3 src/evaluator.c3 src/object.c3 src/environment.c3 src/ascii.c3 src/builtins.c3 src/quote_unquote.c3 src/macro.c3 src/modify.c3
-# c3c compile-only scripts/repl.c3 src/lexer.c3 src/parser.c3 src/ast.c3 src/evaluator.c3 src/object.c3 src/environment.c3 src/ascii.c3 src/builtins.c3 src/quote_unquote.c3 src/macro.c3 src/modify.c3
+c3c compile-run scripts/repl.c3 src/lexer.c3 src/parser.c3 src/ast.c3 src/evaluator.c3 src/runtime.c3 src/environment.c3 src/ascii.c3 src/builtins.c3 src/quote_unquote.c3 src/macro.c3 src/modify.c3
+# c3c compile-only scripts/repl.c3 src/lexer.c3 src/parser.c3 src/ast.c3 src/evaluator.c3 src/runtime.c3 src/environment.c3 src/ascii.c3 src/builtins.c3 src/quote_unquote.c3 src/macro.c3 src/modify.c3
 
